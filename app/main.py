feb6906dadfc0616c9b2b6775da1315f720f0b85
James McKeown <jmckeown@watscoventures.com>
2024-07-23 08:21:02 -0600

    first commit

---
+++ app/main.py
@@ -0,0 +1,63 @@
+import boto3
+import os
+import time
+from time import sleep
+
+region_name = "us-east-1"
+bucket = os.getenv('BUCKET_NAME')
+bucket_audio_path = os.getenv("BUCKET_AUDIO_PATH")
+bucket_transcript_path = os.getenv("BUCKET_TRANSCRIPT_PATH")
+boto3.setup_default_session(region_name=region_name)
+session = boto3.session.Session()
+s3 = session.client(service_name='s3', region_name=region_name)
+#s3 ls s3://gpt-api-temp/transcription-queue/
+transcription_queue = s3.list_objects_v2(Bucket=bucket, Prefix=bucket_audio_path)
+
+if 'Contents' not in transcription_queue:
+    print("No audio files to transcribe")
+    sleep(120)
+    os.system("sudo shutdown")
+else:
+    print(f"Found {len(transcription_queue['Contents'])} files to transcribe")
+print('about to import transcribe_audio')
+t1 = time.time()
+# from .whisperPipe import transcribe_audio
+# from .diarizePipe import diarize
+from .wx2 import process_audio, transcription_model, diarization_model, device, batch_size
+print(time.time() - t1, 'seconds to import transcribe_audio and diarize')
+
+
+def processObj(obj):
+    uuid = obj['Key'].split('/')[-1]
+    audio_path = obj['Key']
+    #copy the audio file to the local filesystem
+    s3.download_file(bucket, audio_path, f"/tmp/{uuid}")
+    print(f"Transcribing {audio_path}")
+    t1 = time.time()
+    transcription = process_audio(f"/tmp/{uuid}", transcription_model, diarization_model, device, batch_size)
+    t2 = time.time()
+    print(f"Transcription took {t2 - t1} seconds")
+    print(f"Transcription: {transcription}")
+    #write the transcription output to a local file
+    with open(f"/tmp/{uuid}.txt", "w", encoding='utf-8') as file:
+        file.write(transcription)
+    #upload the transcription output to S3
+    s3.upload_file(f"/tmp/{uuid}.txt", bucket, f"{bucket_transcript_path}/{uuid}")
+    #delete the local file
+    os.remove(f"/tmp/{uuid}.txt")
+    #delete the local audio file
+    os.remove(f"/tmp/{uuid}")
+    #delete the object from the queue
+    s3.delete_object(Bucket=bucket, Key=audio_path)
+    print(f"Transcription {audio_path} complete")
+
+
+#check if anything new has arrived since we started
+while True:
+    transcription_queue = s3.list_objects_v2(Bucket=bucket, Prefix=bucket_audio_path)
+    if 'Contents' not in transcription_queue:
+        print('nothing left to process, shutting down')
+        #shut down computer
+        os.system("sudo shutdown now")
+    for obj in transcription_queue['Contents']:
+        processObj(obj)
---
+++ app/wx2.py
@@ -0,0 +1,76 @@
+import whisperx
+import os
+import time
+
+def load_models(device, compute_type):
+    print('Loading transcription model...')
+    t_start = time.time()
+    transcription_model = whisperx.load_model("large-v3", device, compute_type=compute_type)
+    print(f'Transcription model loaded in {time.time() - t_start:.2f} seconds.')
+    
+    print('Loading diarization model...')
+    t_start = time.time()
+    diarization_model = whisperx.DiarizationPipeline(model_name="pyannote/speaker-diarization-3.1", use_auth_token=os.getenv('HF_TOKEN'), device=device)
+    print(f'Diarization model loaded in {time.time() - t_start:.2f} seconds.')
+    
+    return transcription_model, diarization_model
+
+def load_alignment_model(language_code, device):
+    print(f'Loading alignment model for language: {language_code}...')
+    t_start = time.time()
+    align_model, metadata = whisperx.load_align_model(language_code=language_code, device=device)
+    print(f'Alignment model loaded in {time.time() - t_start:.2f} seconds.')
+    return align_model, metadata
+
+def process_audio(audio_path, transcription_model, diarization_model, device, batch_size):
+    print('Loading audio...')
+    t_start = time.time()
+    audio = whisperx.load_audio(audio_path)
+    print(f'Audio loaded in {time.time() - t_start:.2f} seconds.')
+    
+    print('Transcribing audio...')
+    t_start = time.time()
+    transcribe_result = transcription_model.transcribe(audio, batch_size=batch_size)
+    print(f'Transcription completed in {time.time() - t_start:.2f} seconds.')
+
+    # Load alignment model based on detected language
+    align_model, metadata = load_alignment_model(transcribe_result["language"], device)
+
+    print('Aligning...')
+    t_start = time.time()
+    aligned_result = whisperx.align(transcribe_result["segments"], align_model, metadata, audio, device, return_char_alignments=False)
+    print(f'Alignment completed in {time.time() - t_start:.2f} seconds.')
+
+    print('Diarizing...')
+    t_start = time.time()
+    diarize_segments = diarization_model(audio)
+    print(f'Diarization completed in {time.time() - t_start:.2f} seconds.')
+    
+    print('Assigning speakers...')
+    t_start = time.time()
+    final_result = whisperx.assign_word_speakers(diarize_segments, aligned_result)
+    print(f'Speaker assignment completed in {time.time() - t_start:.2f} seconds.')
+
+    return format_segments(final_result)
+
+def format_segments(result):
+    formatted_output = ''
+    for segment in result["segments"]:
+        start_minutes = int(segment['start'] // 60)
+        start_seconds = int(segment['start'] % 60)
+        end_minutes = int(segment['end'] // 60)
+        end_seconds = int(segment['end'] % 60)
+        speaker = segment.get('speaker', 'Unknown')
+        print(segment)
+        formatted_output += f"[{start_minutes}:{start_seconds:02d} -> {end_minutes}:{end_seconds:02d}]({speaker}) {segment['text']}\n"
+    return formatted_output
+
+# Set up
+device = "cuda" 
+batch_size = 16
+compute_type = "float16"
+
+# Load transcription and diarization models at the beginning
+transcription_model, diarization_model = load_models(device, compute_type)
+
+
